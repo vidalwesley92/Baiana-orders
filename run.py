0b03c59e649552140c598192cafcd350104cb6c6
wesley vidal <vidalwesley92@gmail.com>
2024-05-31 09:22:42 +0000

    attempt two

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('baiana-orders')
+SHEET = GSPREAD_CLIENT.open('Baiana-orders')
 
 sales = SHEET.worksheet('orders')
 
