29ea05219d599bca85c546fd91c3f91ed34c1309
wesley vidal <vidalwesley92@gmail.com>
2024-05-31 08:55:54 +0000

    add title to fucntion

--- run.py
+++ run.py
@@ -10,32 +10,37 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Baiana-orders')
+SHEET = GSPREAD_CLIENT.open('baiana-orders')
 
-#sales = SHEET.worksheet('orders')
+sales = SHEET.worksheet('orders')
 
-#data = sales.get_all_values()
+data = sales.get_all_values()
 
 #print(data)
 
+def print_title():
+    print("""
+    888888b.            d8b                            d8b                
+    888  "88b           Y8P                            88P                
+    888  .88P                                          8P                 
+    8888888K.   8888b.  888  8888b.  88888b.   8888b.  "   .d8888b        
+    888  "Y88b     "88b 888     "88b 888 "88b     "88b     88K            
+    888    888 .d888888 888 .d888888 888  888 .d888888     "Y8888b.       
+    888   d88P 888  888 888 888  888 888  888 888  888          X88       
+    8888888P"  "Y888888 888 "Y888888 888  888 "Y888888      88888P'       
+                        888                                             
+                        888                                             
+                        888                                             
+    .d88b.  888d888  .d88888  .d88b.  888d888 .d8888b                    
+    d88""88b 888P"   d88" 888 d8P  Y8b 888P"   88K                        
+    888  888 888     888  888 88888888 888     "Y8888b.                   
+    Y88..88P 888     Y88b 888 Y8b.     888          X88                   
+    "Y88P"  888      "Y88888  "Y8888  888      88888P'  
+    """)
 
-print("""
-888888b.            d8b                            d8b                
-888  "88b           Y8P                            88P                
-888  .88P                                          8P                 
-8888888K.   8888b.  888  8888b.  88888b.   8888b.  "   .d8888b        
-888  "Y88b     "88b 888     "88b 888 "88b     "88b     88K            
-888    888 .d888888 888 .d888888 888  888 .d888888     "Y8888b.       
-888   d88P 888  888 888 888  888 888  888 888  888          X88       
-8888888P"  "Y888888 888 "Y888888 888  888 "Y888888      88888P'       
-                      888                                             
-                      888                                             
-                      888                                             
- .d88b.  888d888  .d88888  .d88b.  888d888 .d8888b                    
-d88""88b 888P"   d88" 888 d8P  Y8b 888P"   88K                        
-888  888 888     888  888 88888888 888     "Y8888b.                   
-Y88..88P 888     Y88b 888 Y8b.     888          X88                   
- "Y88P"  888      "Y88888  "Y8888  888      88888P'  
-""")
+    print("\nWelcome to Baiana's Ordering System! Here, you can order some of our delicious food. How can we serve you today?")
 
-print("\nWelcome to Baiana's Ordering System! Here, you can order some of our delicious food. How can we serve you today?")
+def main():
+    print_title()
+
+main()
